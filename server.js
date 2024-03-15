ec61b8c40b529eed1c3fd615dbe91fc7a10b5912
blockbrossnakegame <159715270+blockbrossnakegame@users.noreply.github.com>
2024-03-15 21:22:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ let Discord = require("discord.js");
 let client = new Discord.Client();
 let { MessageEmbed } = require("discord.js");
 const express = require("express");
-const keep_alive = require('./keep_alive.js');
 const app = express();
 
 app.listen(3000, () => {
